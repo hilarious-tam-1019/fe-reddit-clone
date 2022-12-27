a9953437b9b30ba29fc7bbd689009319b8464628
funnytam1019 <tam.nguyen.10119@gmail.com>
2022-12-27 18:07:19 +0700

    update: adding components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { useQuery, gql } from '@apollo/client';
 
 function App() {
   return (
-  )  
+    <div className="flex h-screen overflow-hidden">
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden"></div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header className="sticky top-0 bg-white border-b z-30">
+      <div className="px-4 sm:px-6 lg:px-8"></div>
+    </header>
+  );
+};
